afa1d7abd137c9ee9e9c8e09409ac0bb2e2907cf
Boris Cherny <boris@performancejs.com>
2015-03-29 21:52:52 -0700

    fix zipWith

--- 03.23-zipWith.scala
+++ 03.23-zipWith.scala
@@ -1,17 +1,16 @@
 
 def zipWith [A,B](as: List[A], bs: List[A])(f: (A,A)=>B): List[B] = {
 
-  def loop [A,B](as: List[A], bs: List[A], acc: List[B]): List[B] = (as,bs) match {
+  def loop [A,B](as: List[A], bs: List[A], acc: List[B], f: (A,A)=>B): List[B] = (as,bs) match {
     case (Nil, _) => acc
     case (_, Nil) => acc
-    case (a::as, b::bs) => loop(as, bs, acc :+ f(a, b))
+    case (a::as, b::bs) => loop(as, bs, acc :+ f(a, b), f)
   }
 
-  loop(as, bs, Nil:List[B])
+  loop(as, bs, Nil:List[B], f)
 
 }
 
-assert(zipWith(List(), List())(_+_), List())
 assert(zipWith(List(1,2,3), List(4,5,6))(_+_), List(5,7,9))
 assert(zipWith(List("a", "b", "c"), List("d", "e", "f"))(_+_), List("ad", "be", "cf"))
 
