6c7235f78526fc896487445c3db08063d581d3c9
Boris Cherny <bcherny@gmail.com>
2015-03-30 22:19:06 -0700

    refactor zipWith

--- 03.23-zipWith.scala
+++ 03.23-zipWith.scala
@@ -1,18 +1,18 @@
 
 def zipWith [A,B](as: List[A], bs: List[A])(f: (A,A)=>B): List[B] = {
 
-  def loop [A,B](as: List[A], bs: List[A], acc: List[B], f: (A,A)=>B): List[B] = (as,bs) match {
-    case (Nil, _) => acc
-    case (_, Nil) => acc
-    case (a::as, b::bs) => loop(as, bs, acc :+ f(a, b), f)
+  def loop (as: List[A], bs: List[A], acc: List[B]): List[B] = (as,bs) match {
+    case (a::as, b::bs) => loop(as, bs, acc :+ f(a, b))
+    case (_) => acc
   }
 
-  loop(as, bs, Nil:List[B], f)
+  loop(as, bs, Nil:List[B])
 
 }
 
 assert(zipWith(List(1,2,3), List(4,5,6))(_+_), List(5,7,9))
 assert(zipWith(List("a", "b", "c"), List("d", "e", "f"))(_+_), List("ad", "be", "cf"))
+assert(zipWith(List("a", "b", "c", "d"), List("d", "e", "f"))(_+_), List("ad", "be", "cf"))
 
 def assert(a: Any, b: Any) {
   if (a == b)
