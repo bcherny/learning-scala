7fedc058fced9c3221091c4a1062a6ebbba60d11
Boris Cherny <bcherny@gmail.com>
2016-05-22 11:28:21 -0700

    add 5.6 (wip)

---
+++ 05.06-headOption.scala
@@ -0,0 +1,39 @@
+sealed trait Stream[+A] {
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+  def headOption(): Option[A] =
+    foldRight(None)((a, b) => a match {
+      case Empty => None
+      case _ => Some(_)
+    })
+
+  def toList: List[A] = this match {
+    case Cons(h, t) => h()::t().toList
+    case Empty => Nil
+  }
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+  def empty[A]: Stream[A] = Empty
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+}
+
+assert(Stream.apply(1, 2, 3, 4).headOption, Some(1))
+
+def assert(a: Any, b: Any) {
+  if (a == b)
+    println(s"assertion successful - $a equals $b")
+  else
+    println(s"assertion failed - expected $a to equal $b")
+}
