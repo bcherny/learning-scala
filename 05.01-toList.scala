793556c796fcbdd7f84fb49d9bb21716379132ce
Boris Cherny <bcherny@gmail.com>
2016-05-21 17:16:25 -0700

    5.1

---
+++ 05.01-toList.scala
@@ -0,0 +1,29 @@
+sealed trait Stream[+A] {
+  def toList: List[A] = this match {
+    case Cons(h, t) => h()::t().toList
+    case Empty => Nil
+  }
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+  def empty[A]: Stream[A] = Empty
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+}
+
+assert(Stream.apply().toList, List())
+assert(Stream.apply(1, 2, 3, 4).toList, List(1, 2, 3, 4))
+
+def assert(a: Any, b: Any) {
+  if (a == b)
+    println(s"assertion successful - $a equals $b")
+  else
+    println(s"assertion failed - expected $a to equal $b")
+}
